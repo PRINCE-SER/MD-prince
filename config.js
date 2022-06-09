78286a77a35a538a18f0a48c36fb866bf0dbb6a2
Prince ser <98514119+PRINCE-SER@users.noreply.github.com>
2022-06-09 07:49:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-//═══════[© 2022 Xeon Bot Inc.]════════\\
+//═══════[© 2022 prince Bot Inc.]════════\\
 
 //~you can re-upload but tag my channel
 //or put my channel link in the description.
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['916909137213','6283839980451'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['916909137213'] //another owner number
-global.premium = ['916909137213'] //premium number
-global.pengguna = 'Xeon' //username
-global.botnma = 'Cheems Bot-MD' //bot name
-global.ownernma = '🦄Dream Guy Xeon' //owner name
-global.packname = 'Cheems Bot' //sticker package name
-global.author = 'By Dream Guy Xeon' //sticker author name
+global.owner = ['918590565968','919446652312'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['918590565968'] //another owner number
+global.premium = ['918590565968'] //premium number
+global.pengguna = 'prince' //username
+global.botnma = 'MD-prince' //bot name
+global.ownernma = 'prince ser' //owner name
+global.packname = 'MD-prince' Bot' //sticker package name
+global.author = 'By prince' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '⭔' //design
